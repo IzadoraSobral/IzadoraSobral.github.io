ef84d7581ff476311f2553111f17ed09124a277c
IzadoraSobral <150820135+IzadoraSobral@users.noreply.github.com>
2023-11-17 20:59:36 -0200

    tirei a variavel

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+const filmes=[
+    {
+        nome:'Meu Tio da América',
+        img:"meu-tio-da-america.webp",
+    },  {
+        nome:'Stalker',
+        img:"stalker.jpg",
+    }, 
+]
 var listaFilmes = [
-    "https://leiturafilmica.com.br/wp-content/uploads/2020/10/meu-tio-da-america-poster.jpg",
-    'https://www.circuitospcine.com.br/site/wp-content/uploads/2019/06/stalker.jpg',
-    'https://assets.mubicdn.net/images/notebook/post_images/34295/images-w1400.jpeg?1635384771',
-    'https://upload.wikimedia.org/wikipedia/pt/1/1d/Harry_Potter_Pedra_Filosofal_2001.jpg',
-    'https://prod-ripcut-delivery.disney-plus.net/v1/variant/star/79ED0C68B718692E371392FD5E57336B92B9BAE2CB91F519119CB5910C6C114C/scale?width=506&aspectRatio=2.00&format=jpeg'
-  ];
-  
-  var nomesFilmes = [
+    "meu-tio-da-america.webp",
+    'stalker.jpg',
+    'playtime.jpeg',
+    'harry-potter.jpg',
+    'o-poderoso-chefao-ii.jpg',
+];
+
+var nomesFilmes = [
     "Meu Tio da América",
     "Stalker",
     "Playtime",
     "Harry Potter e a Pedra Filosofal",
     "O Poderoso Chefão II"
-  ];
-  
-  var filmesAdicionados = {};
-  
-  for (var i = 0; i < listaFilmes.length; i++) {
-    if (!filmesAdicionados[nomesFilmes[i]]) {
-      document.write('<div><img src=' + listaFilmes[i] + '><p>' + nomesFilmes[i] + '</p></div>');
-  
-      filmesAdicionados[nomesFilmes[i]] = true;
-    }
-  }
-  
+];
+
+var filmesAdicionados = {};
+
+for (var i = 0; i < filmes.length; i++) {
+ document.write('<div><img src=' + filmes[i].img + '><p>' + filmes[i].nome + '</p></div>');
+
+    
+    
+}
