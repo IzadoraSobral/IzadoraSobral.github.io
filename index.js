9352ea98a406d47e9da4255cc200b6ebe54d5a61
IzadoraSobral <150820135+IzadoraSobral@users.noreply.github.com>
2023-11-20 12:21:13 -0200

    mudei comando

--- index.js
+++ index.js
@@ -5,24 +5,11 @@ const filmes=[
     },  {
         nome:'Stalker',
         img:"stalker.jpg",
+        nome:'Playtime',
+        img:"playtime.jpeg",
+        
     }, 
 ]
-var listaFilmes = [
-    "meu-tio-da-america.webp",
-    'stalker.jpg',
-    'playtime.jpeg',
-    'harry-potter.jpg',
-    'o-poderoso-chefao-ii.jpg',
-];
-
-var nomesFilmes = [
-    "Meu Tio da América",
-    "Stalker",
-    "Playtime",
-    "Harry Potter e a Pedra Filosofal",
-    "O Poderoso Chefão II"
-];
-
 var filmesAdicionados = {};
 
 for (var i = 0; i < filmes.length; i++) {
